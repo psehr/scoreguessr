9d0796347937d289ce02f27723dee664023a787d
psehr <loic.abel@hotmail.com>
2025-02-18 11:01:35 +0100

    even handler for attempts, basic validation & table css layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export type Score = {
   player_name: string;
@@ -14,7 +14,7 @@ export type Score = {
 };
 
 export type ScoreDraft = {
-  try: number;
+  attempt: number;
   score: Score;
 };
 
@@ -23,14 +23,14 @@ const CORRECT_SCORE: Score = {
   player_id: 2324,
   beatmap_name: "xi - Blue Zenith [FOUR DIMENSIONS]",
   beatmap_id: 727,
-  year: 2014,
+  year: 2016,
   pp: 727,
   day: 0,
 };
 
 export default function Home() {
-  const [scoreDraft, setScoreDraft] = useState({
-    try: 0,
+  const initialScoreDraft = {
+    attempt: 0,
     score: {
       player_name: "?",
       player_id: 0,
@@ -40,31 +40,82 @@ export default function Home() {
       pp: CORRECT_SCORE.pp,
       day: 0,
     },
-  } as ScoreDraft);
+  };
 
-  const renderRows = (scoreDrafts: ScoreDraft[]) => {
-    let rows: React.ReactNode[] = [];
-    scoreDrafts.forEach((scoreDraft) => {
-      rows.push(
-        <tr className="h-12">
-          <td className="text-red-400">{scoreDraft.score.player_name}</td>
-          <td className="text-red-400">{scoreDraft.score.beatmap_name}</td>
-          <td className="text-red-400">{scoreDraft.score.year || "?"}</td>
-          <td className="text-green-400">{scoreDraft.score.pp}</td>
+  const [isValids, setIsValids] = useState<{
+    isValidPlayer: boolean;
+    isValidMap: boolean;
+    isValidYear: boolean;
+  }>({
+    isValidPlayer: false,
+    isValidMap: false,
+    isValidYear: false,
+  });
+
+  const [scoreDrafts, setScoreDrafts] = useState<ScoreDraft[]>([
+    initialScoreDraft,
+  ]);
+
+  useEffect(() => {
+    console.log(scoreDrafts);
+    const lastDraft = scoreDrafts[scoreDrafts.length - 1];
+    setIsValids({
+      isValidPlayer: lastDraft.score.player_name === CORRECT_SCORE.player_name,
+      isValidMap: lastDraft.score.beatmap_name === CORRECT_SCORE.beatmap_name,
+      isValidYear: lastDraft.score.year === CORRECT_SCORE.year,
+    });
+  }, [scoreDrafts]);
+
+  const renderRows = () => {
+    return scoreDrafts.map((scoreDraft) => {
+      const isValidPlayer =
+        scoreDraft.score.player_name.toLowerCase() ===
+        CORRECT_SCORE.player_name.toLowerCase();
+      const isValidMap =
+        scoreDraft.score.beatmap_name.toLowerCase() ===
+        CORRECT_SCORE.beatmap_name.toLowerCase();
+      const isValidYear = scoreDraft.score.year === CORRECT_SCORE.year;
+      const isValidPP = scoreDraft.score.pp === CORRECT_SCORE.pp;
+
+      return (
+        <tr className="h-12" key={scoreDraft.attempt}>
+          <td
+            key={`player_name-${scoreDraft.attempt}`}
+            className={isValidPlayer ? "text-green-400" : "text-red-400"}
+          >
+            {scoreDraft.score.player_name}
+          </td>
+          <td
+            key={`beatmap_name-${scoreDraft.attempt}`}
+            className={isValidMap ? "text-green-400" : "text-red-400"}
+          >
+            {scoreDraft.score.beatmap_name}
+          </td>
+          <td
+            key={`year-${scoreDraft.attempt}`}
+            className={isValidYear ? "text-green-400" : "text-red-400"}
+          >
+            {scoreDraft.score.year || "?"}
+          </td>
+          <td
+            key={`pp-${scoreDraft.attempt}`}
+            className={isValidPP ? "text-green-400" : "text-red-400"}
+          >
+            {scoreDraft.score.pp}
+          </td>
         </tr>
       );
     });
-    return rows;
   };
 
   return (
-    <div className="w-full h-full flex flex-col place-content-center items-center space-y-4">
+    <div className="w-full h-full flex flex-col place-content-start items-center space-y-4">
       <div className="text-6xl font-bold p-8">
         <p>Scoreguessr</p>
       </div>
-      <div className="w-2/3 h-fit max-h-2/3 p-8 bg-slate-900 rounded-xl text-center place-content-start items-center">
-        <table className="table-fixed w-full h-fit font-bold">
-          <thead className="text-2xl">
+      <div className="w-2/3 min-h-fit h-2/3 px-8 text-center place-content-start items-center overflow-auto">
+        <table className="table-fixed w-full h-fit bg-slate-900  rounded-xl font-bold">
+          <thead className="h-12 text-2xl">
             <tr>
               <th className="p-2">Player</th>
               <th className="p-2">Beatmap</th>
@@ -72,18 +123,96 @@ export default function Home() {
               <th className="p-2">PP</th>
             </tr>
           </thead>
-          <tbody className="text-lg">{renderRows([scoreDraft])}</tbody>
+          <tbody className="text-lg h-fit">{renderRows()}</tbody>
         </table>
       </div>
       <div className="w-2/3 h-24 p-8 flex flex-row rounded-xl text-center items-center space-x-4">
         <div className="w-1/4 h-12">
-          <input type="text" placeholder="Guess player.." />
+          <form
+            className="w-full h-full"
+            onSubmit={(e) => {
+              e.preventDefault();
+              const guessedPlayer = (
+                e.target as HTMLFormElement
+              ).elements.namedItem("player") as HTMLInputElement;
+              const player = guessedPlayer.value;
+              setScoreDrafts((prevDrafts) => {
+                const lastDraft = prevDrafts[prevDrafts.length - 1];
+                return [
+                  ...prevDrafts,
+                  {
+                    ...lastDraft,
+                    attempt: lastDraft.attempt + 1,
+                    score: {
+                      ...lastDraft.score,
+                      player_name: player,
+                    },
+                  },
+                ];
+              });
+              guessedPlayer.value = "";
+            }}
+          >
+            <input type="text" name="player" placeholder="Guess player.." />
+          </form>
         </div>
         <div className="w-1/2 h-12">
-          <input type="text" placeholder="Guess map.." />
+          <form
+            className="w-full h-full"
+            onSubmit={(e) => {
+              e.preventDefault();
+              const guessedBeatmap = (
+                e.target as HTMLFormElement
+              ).elements.namedItem("beatmap") as HTMLInputElement;
+              const beatmap = guessedBeatmap.value;
+              setScoreDrafts((prevDrafts) => {
+                const lastDraft = prevDrafts[prevDrafts.length - 1];
+                return [
+                  ...prevDrafts,
+                  {
+                    ...lastDraft,
+                    attempt: lastDraft.attempt + 1,
+                    score: {
+                      ...lastDraft.score,
+                      beatmap_name: beatmap,
+                    },
+                  },
+                ];
+              });
+              guessedBeatmap.value = "";
+            }}
+          >
+            <input type="text" name="beatmap" placeholder="Guess map.." />
+          </form>
         </div>
         <div className="w-1/4 h-12">
-          <input type="text" placeholder="Guess year.." />
+          <form
+            className="w-full h-full"
+            onSubmit={(e) => {
+              e.preventDefault();
+              const guessedYear = (
+                e.target as HTMLFormElement
+              ).elements.namedItem("year") as HTMLInputElement;
+              const year = parseInt(guessedYear.value, 10);
+              setScoreDrafts((prevDrafts) => {
+                const lastDraft = prevDrafts[prevDrafts.length - 1];
+                return [
+                  ...prevDrafts,
+                  {
+                    ...lastDraft,
+                    attempt: lastDraft.attempt + 1,
+                    score: {
+                      ...lastDraft.score,
+                      year: year,
+                    },
+                  },
+                ];
+              });
+              guessedYear.value = "";
+            }}
+          >
+            <input type="text" name="year" placeholder="Guess year.." />
+          </form>
         </div>
       </div>
       <div className="absolute bottom-2 text-gray-400">

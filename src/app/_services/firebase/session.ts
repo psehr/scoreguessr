d928d06a5c5c593a202e8bc37a6833845074b718
psehr <loic.abel@hotmail.com>
2025-02-24 12:02:39 +0100

    fixing firestore service account deployment

--- src/app/_services/firebase/session.ts
+++ src/app/_services/firebase/session.ts
@@ -1,9 +1,14 @@
 import * as admin from "firebase-admin";
-var serviceAccount = require("@/serviceAccountKey.json");
+// var serviceAccount = require("@/serviceAccountKey.json");
+const { privateKey } = JSON.parse(process.env.DB_PRIVATE_KEY!);
 
 let app = admin.initializeApp(
   {
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      clientEmail: process.env.DB_CLIENT_EMAIL,
+      privateKey: privateKey,
+      projectId: process.env.DB_PROJECT_ID,
+    }),
   },
   Date.now().toString()
 );

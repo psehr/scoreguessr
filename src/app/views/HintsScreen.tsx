50bdc1d4032d55f6849b6b8070bfd3edd00b0e73
psehr <loic.abel@hotmail.com>
2025-03-01 22:00:48 +0100

    adding mod icons support

--- src/app/views/HintsScreen.tsx
+++ src/app/views/HintsScreen.tsx
@@ -1,4 +1,61 @@
+import { Mod } from "osu-api-extended";
 import { GuessableScore, ScoreDraft } from "../types";
+
+import Image from "next/image";
+import dt from "../../../public/mod_icons/dt.png";
+import ez from "../../../public/mod_icons/ez.png";
+import fl from "../../../public/mod_icons/fl.png";
+import hd from "../../../public/mod_icons/hd.png";
+import hr from "../../../public/mod_icons/hr.png";
+import ht from "../../../public/mod_icons/ht.png";
+import nc from "../../../public/mod_icons/nc.png";
+import nf from "../../../public/mod_icons/nf.png";
+import pf from "../../../public/mod_icons/pf.png";
+import sd from "../../../public/mod_icons/sd.png";
+import so from "../../../public/mod_icons/so.png";
+
+export function getModIcon(mod: Mod) {
+  let selectedModFile;
+  switch (mod.acronym) {
+    case "DT":
+      selectedModFile = dt;
+      break;
+    case "EZ":
+      selectedModFile = ez;
+      break;
+    case "FL":
+      selectedModFile = fl;
+      break;
+    case "HD":
+      selectedModFile = hd;
+      break;
+    case "HR":
+      selectedModFile = hr;
+      break;
+    case "HT":
+      selectedModFile = ht;
+      break;
+    case "NC":
+      selectedModFile = nc;
+      break;
+    case "NF":
+      selectedModFile = nf;
+      break;
+    case "PF":
+      selectedModFile = pf;
+      break;
+    case "SD":
+      selectedModFile = sd;
+      break;
+    case "SO":
+      selectedModFile = so;
+      break;
+    default:
+      break;
+  }
+
+  return selectedModFile;
+}
 
 export default function HintsScreen({
   setSelectedView,
@@ -37,11 +94,18 @@ export default function HintsScreen({
             Hint 2: Mods
           </p>
           {everyDrafts.length >= 5 ? (
-            <div className="flex flex-row space-x-2 text-xl p-1 px-4 bg-slate-500">
+            <div className="flex flex-row space-x-1 text-xl p-1 px-4 bg-slate-500">
               <p>Used mods: </p>
-              {validScore.mods?.map((mod) => (
-                <p key={mod.acronym}>{mod.acronym}</p>
-              ))}
+              {validScore.mods?.map((mod) =>
+                getModIcon(mod) ? (
+                  <Image
+                    key={mod.acronym}
+                    src={getModIcon(mod)!}
+                    alt="mod-icon"
+                    className="scale-75"
+                  />
+                ) : null
+              )}
               {!validScore.mods?.length ? <p>None</p> : null}
             </div>
           ) : (

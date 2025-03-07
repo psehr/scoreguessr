bde7e70e525549d34dd7b3d596f6b956a9226cbb
psehr <loic.abel@hotmail.com>
2025-03-07 18:13:40 +0100

    fixing yet another mobile height overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
     <html lang="en">
       <SessionProvider session={session}>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased text-white w-screen h-screen relative overflow-hidden `}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased text-white w-dvw md:w-screen h-dvh md:h-screen relative`}
         >
           {children}
           <Image

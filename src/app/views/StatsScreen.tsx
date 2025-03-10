3f6e5288a1f913f41d89b61215dd4183a583b928
psehr <loic.abel@hotmail.com>
2025-03-10 19:16:31 +0100

    fixing responsiveness on stats screen

--- src/app/views/StatsScreen.tsx
+++ src/app/views/StatsScreen.tsx
@@ -55,10 +55,10 @@ export default function StatsScreen({
       onClick={() => setSelectedView("Default")}
     >
       <div
-        className="rounded-xl flex flex-col md:flex-row place-content-center items-center w-full md:w-1/2 h-fit min-h-[60%] md:min-h-[40%] p-4 space-y-2 md:space-x-2 md:space-y-0"
+        className="rounded-xl flex flex-col md:flex-row place-content-center items-center w-full md:w-1/2 h-full md:h-fit min-h-[80%] md:min-h-[40%] p-4 space-y-2 md:space-x-2 md:space-y-0"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex flex-col w-1/3 h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
+        {/* <div className="flex flex-col w-full h-1/3 md:w-1/3 md:h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
           <div className="flex flex-col space-y-8 h-full w-full place-content-start items-center">
             <div className="h-1/4 font-bold flex flex-col place-content-center items-center text-2xl space-y-4">
               <p className="font-semibold underline bg-black/30 pb-1 px-8 rounded-xl shadow-lg">
@@ -70,15 +70,15 @@ export default function StatsScreen({
             </div>
             <div className="h-1/2 font-bold flex flex-col place-content-center items-center text-2xl space-y-4"></div>
           </div>
-        </div>
-        <div className="border border-green-400 flex flex-col w-1/3 h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
+        </div> */}
+        <div className="border border-green-400 flex flex-col w-full h-1/3 md:w-1/3 md:h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
           <div className="flex flex-col px-8 h-full w-full place-content-start items-center">
-            <div className="h-1/5 font-bold flex flex-col place-content-center items-center text-2xl space-y-4">
+            <div className="h-1/5 font-bold flex flex-col place-content-center items-center text-xl md:text-2xl space-y-4">
               <p className="font-semibold underline bg-black/30 pb-1 px-8 rounded-xl shadow-lg">
                 This score
               </p>
             </div>
-            <div className="h-1/2 w-full font-bold flex flex-col place-content-center items-center text-2xl space-y-4 border border-green-600/50 shadow-lg bg-gradient-to-br from-green-800/50 to-green-950/50 rounded-xl">
+            <div className="h-1/2 w-full font-bold flex flex-col place-content-center items-center text-lg md:text-2xl space-y-1 md:space-y-4 border border-green-600/50 shadow-lg bg-gradient-to-br from-green-800/50 to-green-950/50 rounded-xl">
               <div className="flex flex-row space-x-1 place-content-center items-center">
                 <p className="font-bold p-1 px-2 rounded-xl">
                   Day {score.day_index}
@@ -86,7 +86,7 @@ export default function StatsScreen({
                 {foundScore ? <LuSquareCheckBig /> : null}
               </div>
               {isAuthenticated ? (
-                <div className="text-xl font-semibold flex flex-col place-content-center items-center space-y-2">
+                <div className="text-lg md:text-xl font-semibold flex flex-col place-content-center items-center space-y-0 md:space-y-2">
                   {foundScore ? (
                     <>
                       <p className="bg-green-400/30 p-1 px-4 rounded-xl">
@@ -98,7 +98,7 @@ export default function StatsScreen({
                           window.open(`https://osu.ppy.sh/scores/${score.id}`)
                         }
                       >
-                        <p className="underline font-normal text-lg">
+                        <p className="underline font-normal text-base">
                           See score page
                         </p>
                         <LuExternalLink />
@@ -112,7 +112,7 @@ export default function StatsScreen({
                 </div>
               ) : null}
             </div>
-            <div className="h-1/3 flex flex-col place-content-center items-center">
+            <div className="text-base md:text-xl h-1/3 flex flex-col place-content-center items-center">
               <div className="flex flex-row space-x-2 place-content-center items-center">
                 <p>Found</p>
                 <p className="font-bold text-green-400 pt-0.5">X</p>
@@ -125,15 +125,15 @@ export default function StatsScreen({
             </div>
           </div>
         </div>
-        <div className="border border-blue-400 px-8 flex flex-col w-1/3 h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
+        <div className="border border-blue-400 px-8 flex flex-col w-full h-1/3 md:w-1/3 md:h-full bg-white/10 rounded-xl place-content-start items-center text-xl shadow-md">
           {userData && todayScore ? (
             <div className="flex flex-col h-full w-full place-content-center items-center">
-              <div className="h-1/5 font-bold flex flex-col place-content-center items-center text-2xl">
+              <div className="h-1/5 font-bold flex flex-col place-content-center items-center text-xl md:text-2xl">
                 <p className="font-semibold underline bg-black/30 pb-1 px-8 rounded-xl shadow-lg">
                   Your profile
                 </p>
               </div>
-              <div className="h-1/2 w-full font-bold flex flex-col place-content-center items-center text-2xl space-y-4 border border-blue-600/50 shadow-lg bg-gradient-to-br from-blue-800/50 to-slate-950/50 rounded-xl">
+              <div className="h-1/2 w-fit min-w-[60%] md:w-full font-bold flex flex-col place-content-center items-center text-xl md:text-2xl space-y-2 md:space-y-4 border border-blue-600/50 shadow-lg bg-gradient-to-br from-blue-800/50 to-slate-950/50 rounded-xl">
                 <div>
                   <PlayerCard
                     player={{
@@ -146,7 +146,7 @@ export default function StatsScreen({
                 </div>
                 <Flag className="rounded-xl h-10 shadow-lg" />
               </div>
-              <div className="h-1/3 flex flex-col place-content-center items-center">
+              <div className="text-base md:text-xl h-1/3 flex flex-col place-content-center items-center">
                 <div className="flex flex-row space-x-1">
                   <p>Joined</p>
                   <p className="font-bold text-blue-400">
